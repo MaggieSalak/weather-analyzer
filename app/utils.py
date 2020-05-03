e7f61e871e814061527d1674ce32bf88b3997376
Maggie Salak <malgorzata.salak@gmail.com>
2020-05-03 16:38:53 +0200

    Cache weather view function

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 def query_url(url):
+    print('Querying URL')
     resp = requests.get(url=url)
     resp_json = json.dumps(resp.json())
     return json.loads(resp_json)
--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from flask_caching import Cache
 from app import controllers
 
-app = Flask(__name__)             # create an app instance
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+app = Flask(__name__)
+cache.init_app(app)
 
-@app.route('/')                   # at the end point /
+@app.route('/')
+@cache.cached(timeout=300)
 def weather():
     weather_controller = controllers.WeatherController()
     current_weather = weather_controller.get_current_weather()
