069037132993145def1763811a641d62359a0d3a
Maggie Salak <masalak@microsoft.com>
2020-05-09 19:57:10 +0200

    Get weather forecast call

--- app/controllers.py
+++ app/controllers.py
@@ -21,14 +21,25 @@ class WeatherController:
         weather_dict = utils.query_url(url)
         return weather_dict
 
-    def past_weather_url(self):
-        base_url = 'http://api.openweathermap.org/data/2.5/forecast/daily'
-        url = base_url + '?id=' + str(self.cityId) + '&appid=' + self.apiKey
+    def forecast_url(self, weather_data):
+        base_url = 'https://api.openweathermap.org/data/2.5/onecall'
+        url = base_url + '?lat=' + str(weather_data.location.lat) + \
+            '&lon=' + str(weather_data.location.lon) + \
+            '&exclude=current,minutely,hourly' + \
+            '&appid=' + self.apiKey
         print(url)
         return url
+
+    def get_weather_forecast(self, weather_data):
+        url = self.forecast_url(weather_data)
+        weather_dict = utils.query_url(url)
+        return weather_dict
 
     def get_weather(self):
         current_weather_dict = self.get_current_weather()
         print(current_weather_dict)
-        current_weather_data = models.dict_to_weather_data(current_weather_dict)
-        return current_weather_data
+        current_weather = models.dict_to_weather_data(current_weather_dict)
+        weather_forecast = self.get_weather_forecast(current_weather)
+        print('forecast')
+        print(weather_forecast)
+        return current_weather
